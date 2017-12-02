2ef5763c87ac1beb16c07a6b0d1914346f4bac89
Nigel Rahkola <nigel.rahkola@pulseenergy.com>
2017-12-01 21:42:13 -0800

    Day 2 part 1

---
+++ d2/part1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+    with open('in.txt', 'r') as f:
+        sum = 0
+        for line in f:
+            nums = line.strip().split()
+            nums = list(map(int, nums))
+            sum += max(nums) - min(nums)
+        print(sum)
+
+
+if __name__ == '__main__':
+    main()
