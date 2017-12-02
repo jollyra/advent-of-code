aa8ad8624d24c106ff6187fe08bc754a90984e66
Nigel Rahkola <nigel.rahkola@pulseenergy.com>
2017-12-01 21:42:55 -0800

    Add day 2 part 2

---
+++ d2/part2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    str = sys.stdin.read()
+    lines = str.split('\n')
+    sum = 0
+    for line in lines:
+        nums = line.strip().split()
+        nums = list(map(int, nums))
+        for i, x in enumerate(nums):
+            for j, y in enumerate(nums):
+                if i != j and x%y == 0:
+                    sum += x/y
+    print(sum)
+
+
+if __name__ == '__main__':
+    main()
