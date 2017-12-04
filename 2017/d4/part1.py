b9198312d6fd323b99529abbea43de03d614681d
Nigel <me@nigelrahkola.com>
2017-12-03 21:58:46 -0800

    Refactored solutions for d4

--- 2017/d4/part1.py
+++ 2017/d4/part1.py
@@ -1,31 +1,24 @@
 #! /usr/bin/env python3
 
-from util import *
 import sys
 
 
 def main():
     with open(sys.argv[1], 'r') as fh:
-        valid = 0
-        invalid = 0
+        valid_count = 0
         for line in fh:
-            good = True
-            counter = Counter()
-            line = line.strip()
-            words = line.split()
-            for word in words:
-                counter[word] += 1
-            for key, val in counter.items():
-                if val > 1:
-                    good = False
+            valid = True
+            cache = set()
+            words = line.strip().split()
+            for w in words:
+                if w in cache:
+                    valid = False
                     break
-            if good:
-                valid += 1
-            else:
-                invalid += 1
-        print('valid: {} invalid: {}'.format(valid, invalid))
-
-
+                else:
+                    cache.add(w)
+            if valid:
+                valid_count += 1
+        print(valid_count)
 
 
 if __name__ == '__main__':
--- 2017/d4/part2.py
+++ 2017/d4/part2.py
@@ -1,35 +1,27 @@
 #! /usr/bin/env python3
 
-from util import *
 import sys
 
 
 def main():
     with open(sys.argv[1], 'r') as fh:
-        valid = 0
-        invalid = 0
+        valid_count = 0
         for line in fh:
-            good = True
-            counter = Counter()
-            line = line.strip()
-            words = line.split()
-            for word in words:
-                letters = [c for c in word]
-                letters.sort()
-                word = ''.join(letters)
-                counter[word] += 1
-            for key, val in counter.items():
-                print(key, val)
-                if val > 1:
-                    good = False
+            valid = True
+            cache = set()
+            words = line.strip().split()
+            for w in words:
+                w = [c for c in w]
+                w.sort()
+                w = ''.join(w)
+                if w in cache:
+                    valid = False
                     break
-            if good:
-                valid += 1
-            else:
-                invalid += 1
-        print('valid: {} invalid: {}'.format(valid, invalid))
-
-
+                else:
+                    cache.add(w)
+            if valid:
+                valid_count += 1
+        print(valid_count)
 
 
 if __name__ == '__main__':
