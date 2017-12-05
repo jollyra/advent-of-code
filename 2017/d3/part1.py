2a4f874f36550948d46865fadb0d203a725c07ee
Nigel Rahkola <nigel.rahkola@pulseenergy.com>
2017-12-04 22:21:18 -0800

    Use formulaic method instead of generating spiral

--- 2017/d3/part1.py
+++ 2017/d3/part1.py
@@ -2,18 +2,22 @@
 
 
 def main():
-    x = int(input().strip())
-    layer, start = get_layer_and_start(x)
-    print('layer:{} start:{}'.format(layer, start))
-    x0, y0 = get_start_xy(layer)
-    print('start x:{} start y:{}'.format(x0, y0))
-    xf, yf = traverse(x0, y0, start, layer, x)
-    print('final x:{} final y:{}'.format(xf, yf))
-    print('manhattan distance: {}'.format(manhattan_dist(xf, yf)))
+    assert manhattan_dist_to_origin(12) == 3
+    assert manhattan_dist_to_origin(23) == 2
+    assert manhattan_dist_to_origin(1024) == 31
+    assert manhattan_dist_to_origin(312051) == 430
+    ans = manhattan_dist_to_origin(int(input().strip()))
+    print('manhattan distance to origin: {}'.format(ans))
 
 
-def manhattan_dist(x, y):
-    return abs(x) + abs(y)
+def manhattan_dist_to_origin(spiral_index):
+    layer, layer_start_index = get_layer_and_start(spiral_index)
+    side_len = layer * 8 / 4
+    side_midpoint = side_len // 2 - 1  # -1 for 0-indexing
+    side_index = (spiral_index - layer_start_index) % side_len
+    dist_to_axis1 = abs(side_index - side_midpoint)
+    dist_to_axis2 = layer
+    return int(dist_to_axis1 + dist_to_axis2)
 
 
 def get_layer_and_start(x):
@@ -21,52 +25,7 @@ def get_layer_and_start(x):
     while corners[-1] < x:
         next = corners[-1] + 8 * len(corners)
         corners.append(next)
-    print('corners: {}'.format(corners))
     return len(corners) - 1, corners[-2] + 1
-
-
-def get_start_xy(layer):
-    return 1 * layer, -1 * (layer - 1)
-
-
-def traverse(x, y, count, layer, target):
-    cells = cells_in_layer(layer)
-    segment_size = cells // 4
-
-    # up
-    for i in range(segment_size - 1):
-        if count == target:
-            return x, y
-        y += 1
-        count += 1
-
-    # left
-    for i in range(segment_size):
-        if count == target:
-            return x, y
-        x -= 1
-        count += 1
-
-    # down
-    for i in range(segment_size):
-        if count == target:
-            return x, y
-        y -= 1
-        count += 1
-
-    # right
-    for i in range(segment_size):
-        if count == target:
-            return x, y
-        x += 1
-        count += 1
-
-
-def cells_in_layer(layer):
-    if layer == 0:
-        return 1
-    else:
-        return 8 * layer
 
 
 if __name__ == '__main__':
