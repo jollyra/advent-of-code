05bf0c92d280a51ba36ae6a2aad8d03e0e3cd9f1
Nigel <me@nigelrahkola.com>
2017-12-15 22:44:10 -0800

    Move dancing into a function

--- 2017/sixteen.py
+++ 2017/sixteen.py
@@ -8,23 +8,11 @@ from util import *
 cat = ''.join
 
 
-def find_first_cyle(ps, moves):
+def find_first_cycle(ps, moves):
     ps = deque(ps)
     cache = set(letters)
     for i in range(1000000000):
-        for move in moves:
-            if move[0] == 's':
-                ps.rotate(int(move[1:]))
-            elif move[0] == 'x':
-                args = move[1:].split('/')
-                a = int(args[0])
-                b = int(args[1])
-                ps[b], ps[a] = ps[a], ps[b]
-            elif move[0] == 'p':
-                args = move[1:].split('/')
-                a = ps.index(args[0])
-                b = ps.index(args[1])
-                ps[b], ps[a] = ps[a], ps[b]
+        ps = dance(ps, moves)
         if cat(ps) in cache:
             print('repeat!', cat(ps), i)
             return 'win'
@@ -33,7 +21,24 @@ def find_first_cyle(ps, moves):
     return cat(ps)
 
 
+def dance(ps, moves):
+    for move in moves:
+        if move[0] == 's':
+            ps.rotate(int(move[1:]))
+        elif move[0] == 'x':
+            args = move[1:].split('/')
+            a = int(args[0])
+            b = int(args[1])
+            ps[b], ps[a] = ps[a], ps[b]
+        elif move[0] == 'p':
+            args = move[1:].split('/')
+            a = ps.index(args[0])
+            b = ps.index(args[1])
+            ps[b], ps[a] = ps[a], ps[b]
+    return ps
+
+
 if __name__ == '__main__':
     letters = 'abcdefghijklmnop'
     moves = input1_seq(f='16', sep=',')
-    find_first_cyle(letters, moves)
+    find_first_cycle(letters, moves)
