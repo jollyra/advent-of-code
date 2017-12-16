358ca9636d34ee2852c00125f2fab45e4d88c42d
Nigel <me@nigelrahkola.com>
2017-12-15 23:15:50 -0800

    Make script actually output the answer to d16 part 2

--- 2017/sixteen.py
+++ 2017/sixteen.py
@@ -8,37 +8,40 @@ from util import *
 cat = ''.join
 
 
-def find_first_cycle(ps, moves):
-    ps = deque(ps)
+def find_period(seq, moves, rounds):
+    seq = deque(seq)
     cache = set()
-    for i in range(1000000000):
-        ps = dance(ps, moves)
-        if cat(ps) in cache:
-            print('repeat!', cat(ps), i)
-            return 'win'
+    for i in range(rounds):
+        seq = dance(seq, moves)
+        if cat(seq) in cache:
+            print('repeat!', cat(seq), i)
+            return i
         else:
-            cache.add(cat(ps))
-    return cat(ps)
+            cache.add(cat(seq))
+    return cat(seq)
 
 
-def dance(ps, moves):
+def dance(seq, moves):
     for move in moves:
         if move[0] == 's':
-            ps.rotate(int(move[1:]))
+            seq.rotate(int(move[1:]))
         elif move[0] == 'x':
-            args = move[1:].split('/')
-            a = int(args[0])
-            b = int(args[1])
-            ps[b], ps[a] = ps[a], ps[b]
+            a, b = move[1:].split('/')
+            a, b = (int(a), int(b))
+            seq[b], seq[a] = seq[a], seq[b]
         elif move[0] == 'p':
-            args = move[1:].split('/')
-            a = ps.index(args[0])
-            b = ps.index(args[1])
-            ps[b], ps[a] = ps[a], ps[b]
-    return ps
+            a, b = move[1:].split('/')
+            ia, ib = (seq.index(a), seq.index(b))
+            ia, ib = (int(ia), int(ib))
+            seq[ib], seq[ia] = seq[ia], seq[ib]
+    return seq
 
 
 if __name__ == '__main__':
     letters = 'abcdefghijklmnop'
+    one_billion = int(1e9)
     moves = input1_seq(f='16', sep=',')
-    find_first_cycle(letters, moves)
+    period = find_period(letters, moves, one_billion)
+    i = (one_billion % period)
+    seq = find_period(letters, moves, i)
+    print('Letters look like {} after 1 billion rounds of dance'.format(seq))
