4224360a5fa68c0812275f65eddfd08aecc83abf
Nigel <me@nigelrahkola.com>
2018-01-06 14:51:16 -0800

    Add java solution for day1 part 2

--- 2016/java/day1/src/com/company/Main.java
+++ 2016/java/day1/src/com/company/Main.java
@@ -3,24 +3,28 @@ package com.company;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        List<String> testList = new ArrayList<>(Arrays.asList("R8", "R4", "R4", "R8"));
+        Traveller tTest = new Traveller(parse(testList));
+        assert Traveller.howFar(tTest.findFirstRepeatPoint()) == 4;
+
         List<String> list = input();
-        LinkedList<Pair<String, Integer>> dirs = parse(list);
+        ArrayList<Pair<String, Integer>> dirs = parse(list);
         Traveller t = new Traveller(dirs);
-        t.go();
-        int ans = t.manhattanDistanceToOrigin();
-        System.out.println("ans: " + ans);
+
+        Point pa = t.travel();
+        System.out.println("part1: " + Traveller.howFar(pa));
+
+        Point pb = t.findFirstRepeatPoint();
+        System.out.println("part2: " + Traveller.howFar(pb));
     }
 
-    public static LinkedList<Pair<String, Integer>> parse(List<String> raw) {
-        LinkedList pairs = new LinkedList();
+    public static ArrayList<Pair<String, Integer>> parse(List<String> raw) {
+        ArrayList pairs = new ArrayList();
         for (String d : raw) {
             String dir = d.substring(0, 1);
             int num = Integer.parseInt(d.substring(1));
@@ -36,73 +40,122 @@ public class Main {
     }
 }
 
-class Pair<L, R> {
+class Traveller {
+    Heading heading;
+    ArrayList<Pair<String, Integer>> dirs;
+    Point pos = new Point(0, 0);
+    List<Point> visited;
 
-    public L left;
-    public R right;
-
-    public Pair(L left, R right) {
-        this.left = left;
-        this.right = right;
+    public Traveller(ArrayList<Pair<String, Integer>> dirs) {
+        heading = new Heading();
+        this.dirs = dirs;
+        visited = new ArrayList<>();
     }
 
-    public String toString() {
-        return "(" + this.left.toString() + ", " + this.right.toString() + ")";
+    public Point travel() {
+        pos = new Point(0, 0);
+        for (Pair<String, Integer> p : dirs) {
+            if(p.L.equals("L")) {
+                heading.rotL();
+            } else if(p.L.equals("R")) {
+                heading.rotR();
+            } else {
+                throw new Error("unrecognized direction: " + p.L);
+            }
+            pos = Point.add(pos, Point.mul(heading.getHeading(), p.R));
+        }
+        return pos;
+    }
+
+    public static int howFar(Point p) {
+        return Math.abs(p.x) + Math.abs(p.y);
+    }
+
+    public Point findFirstRepeatPoint() {
+        pos = new Point(0, 0);
+        for (Pair<String, Integer> p : dirs) {
+            if(p.L.equals("L")) {
+                heading.rotL();
+            } else if(p.L.equals("R")) {
+                heading.rotR();
+            } else {
+                throw new Error("unrecognized direction: " + p.L);
+            }
+
+            for(int i=0; i<p.R; i++) {
+                pos = Point.add(pos, heading.getHeading());
+                if(visited.contains(pos)) {
+                    return pos;
+                } else {
+                    visited.add(pos);
+                }
+            }
+        }
+        return null;
     }
 }
 
-class Traveller {
-    Heading heading;
-    LinkedList<Pair<String, Integer>> dirs;
-    int[] pos = {0, 0};
+class Pair<L, R> {
 
-    public Traveller(LinkedList<Pair<String, Integer>> dirs) {
-        heading = new Heading();
-        this.dirs = dirs;
+    public L L;
+    public R R;
+
+    public Pair(L left, R right) {
+        L = left;
+        R = right;
     }
 
-    public int[] go() {
-        for (Pair<String, Integer> p : dirs) {
-            System.out.println(p.toString());
-            if(p.left.equals("L")) {
-                heading.rotL();
-            } else if(p.left.equals("R")) {
-                heading.rotR();
-            } else {
-                throw new Error("unrecognized direction: " + p.left);
-            }
-            this.pos = add(this.pos, mul(this.heading.getHeading(), p.right));
-        }
-        return this.pos;
+    public String toString() {
+        return "(" + L.toString() + ", " + R.toString() + ")";
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public int manhattanDistanceToOrigin() {
-        return Math.abs(this.pos[0]) + Math.abs(this.pos[1]);
+    public static Point add(Point a, Point b) {
+        return new Point(a.x + b.x, a.y + b.y);
     }
 
-    public static int[] add(int[] a, int[] b) {
-        return new int[] {a[0] + b[0], a[1] + b[1]};
+    public static Point mul(Point a, int x) {
+        return new Point(x * a.x, x * a.y);
     }
 
-    public static int[] mul(int[] a, int x) {
-        return new int[] {x * a[0], x * a[1]};
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
     }
 }
 
 class Heading {
-    int[] v = {0, 1};
+    Point v;
 
-    public Heading() {}
+    public Heading() {
+         v = new Point(0, 1);
+    }
 
     public void rotR() {
-        this.v = new int[]{v[1], -v[0]};
+        this.v = new Point(v.y, -v.x);
     }
 
     public void rotL() {
-        this.v = new int[]{-v[1], v[0]};
+        this.v = new Point(-v.y, v.x);
     }
 
-    public int[] getHeading() {
+    public Point getHeading() {
         return v;
     }
 }
