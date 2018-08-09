ef43c8f334c6213e3a334e811da36d2e5d417183
Nigel <me@nigelrahkola.com>
2018-08-09 11:41:46 -0400

    Add 2015 day 4 first pass
    Part 1 + part 2 took 29m

---
+++ 2015/python/four.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+import hashlib
+
+
+def mine(secret):
+    salt = 0
+    while 1:
+        plaintext = secret + str(salt)
+        hash = hashlib.md5(plaintext.encode('utf-8')).hexdigest() 
+        if hash[:6] == '000000':
+            return salt
+        salt += 1
+
+
+if __name__ == '__main__':
+    # assert mine('abcdef') == 609043
+    # assert mine('pqrstuv') == 1048970
+    print('pass')
+
+    ans = mine('iwrupvqb')
+    print('ans: {}'.format(ans))
