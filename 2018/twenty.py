d9a0ef50e33930a7aa9f3cf1a802970459e90be8
Nigel Rahkola <me@nigelrahkola.com>
2019-11-22 07:47:27 -0500

    Use lru_cache on an n-ary tree

--- 2018/twenty.py
+++ 2018/twenty.py
@@ -1,5 +1,8 @@
+from functools import lru_cache
+
+
 def neighbours_diagonal(point):
-    'Only diagonals returned in order quadrants I, II, III, IV (CCW).'
+    """Only diagonals returned in order quadrants I, II, III, IV (CCW)."""
     x, y = point
     return [(x + 1, y + 1), (x - 1, y + 1), (x - 1, y - 1), (x + 1, y - 1)]
 
@@ -100,47 +103,98 @@ def find_matching_paren(re, i):
     return -1, []
 
 
-def parse_regex(re):
-    print('parse_regex')
+class Node:
+    count = 0
+
+    def __init__(self):
+        self.v = ''
+        self.N = []
+        self.id = self._get_id()
+
+    def _get_id(self):
+        Node.count += 1
+        return Node.count
+
+    def add_value(self, value):
+        self.v += value
+
+    def add_child(self, node):
+        self.N.append(node)
+
+    def __str__(self):
+        return f'node {self.id} value {self.v} children {[node for node in self.N]}'
+
+    def __repr__(self):
+        return f'{self.id}'
+
+
+def shortest_path_to_farthest_door(re):
     src = (0, 0)
-    coords = {src: 'X'}
+    node = parse_regex(re)
+    # print_preorder_traversal(node)
+    coords = build_map(node)
+    costs = bfs(coords, src)
+    return max(costs.values())
 
-    def parse(re, cur):
-        i = 0
-        while i < len(re):
-            if re[i] == '^':
-                i += 1
-            if re[i] == '$':
-                return
-            if re[i] in 'NSEW':
-                cur = step(coords, cur, re[i])
-                i += 1
-            if re[i] == '(':
-                open_paren_idx = i
-                close_paren_idx, pipe_idxs = find_matching_paren(re, open_paren_idx)
-                if close_paren_idx == -1:
-                    raise Exception(f'Paren match error: no matching close paren found')
 
-                rest_of_re = re[close_paren_idx+1:]
-                if len(pipe_idxs) == 0:
-                    raise Exception(f'Paren match error: no pipes found')
-                else:
-                    p = open_paren_idx
-                    q = 0
-                    while q < len(pipe_idxs):
-                        branch = re[p+1:pipe_idxs[q]]
-                        parse(branch + rest_of_re, cur)
-                        p = pipe_idxs[q]  # Move past the pipe we just used
-                        q += 1
+def print_preorder_traversal(node):
+    print(node)
+    for n in node.N:
+        print_preorder_traversal(n)
 
-                    branch = re[pipe_idxs[-1]+1:close_paren_idx]
-                    parse(branch + rest_of_re, cur)
 
-                return
+def build_map(node):
+    print('build_map')
+    cur = (0, 0)
+    coords = {cur: 'X'}
 
-    parse(re, src)
+    def preorder_traversal(node, current_point):
+        for direction in node.v:
+            current_point = step(coords, current_point, direction)
+        for n in node.N:
+            preorder_traversal(n, current_point)
+
+    preorder_traversal(node, cur)
     fill_unknowns_with_walls(coords)
     return coords
+
+
+@lru_cache(maxsize=None)
+def parse_regex(re):
+    i = 0
+    node = Node()
+    while i < len(re):
+        if re[i] == '^':
+            i += 1
+        if re[i] == '$':
+            print(parse_regex.cache_info())
+            return node
+        if re[i] in 'NSEW':
+            node.add_value(re[i])
+            i += 1
+        if re[i] == '(':
+            open_paren_idx = i
+            close_paren_idx, pipe_idxs = find_matching_paren(re, open_paren_idx)
+            if close_paren_idx == -1:
+                raise Exception(f'Paren match error: no matching close paren found')
+
+            rest_of_re = re[close_paren_idx+1:]
+            if len(pipe_idxs) == 0:
+                raise Exception(f'Paren match error: no pipes found')
+            else:
+                p = open_paren_idx
+                q = 0
+                while q < len(pipe_idxs):
+                    branch = re[p+1:pipe_idxs[q]]
+                    node.add_child(parse_regex(branch + rest_of_re))
+                    p = pipe_idxs[q]  # Move past the pipe we just used
+                    q += 1
+
+                branch = re[pipe_idxs[-1]+1:close_paren_idx]
+                node.add_child(parse_regex(branch + rest_of_re))
+            return node
+
+    return node
 
 
 def bfs(coords, src):
@@ -148,11 +202,7 @@ def bfs(coords, src):
     horizon = [src]
     costs = {src: 0}
     while horizon:
-        # print(len(horizon))
-        # print(f'horizon {horizon}')
-        # print(f'costs {costs}')
         cur = horizon.pop(-1)
-        # print(cur)
         for step_fn in [east, north, west, south]:
             if coords[step_fn(cur)] in '|-':
                 nxt = step_fn(step_fn(cur))
@@ -162,15 +212,7 @@ def bfs(coords, src):
                 else:
                     costs[nxt] = costs[cur] + 1
                     horizon.append(nxt)
-
     return costs
-
-
-def shortest_path_to_farthest_door(re):
-    src = (0, 0)
-    coords = parse_regex(re)
-    costs = bfs(coords, src)
-    return max(costs.values())
 
 
 def test_shortest_path(re, want):
@@ -186,8 +228,8 @@ def main():
     assert(find_matching_paren('^ENWWW(NEEE|SSE(EE|N))$', 6) == (21, [11]))
     assert(find_matching_paren('^ENWWW(NEEE|SSE(EE|N))$', 15) == (20, [18]))
     assert(find_matching_paren('^(NEWS|WNSE|)$', 1) == (12, [6, 11]))
-    assert(test_shortest_path('^ENWWW(NEEE|SSE(EE|N))$', 10))
     assert(test_shortest_path('^WNE$', 3))
+    assert(test_shortest_path('^ENWWW(NEEE|SSE(EE|N))$', 10))
     assert(test_shortest_path('^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$', 18))
     assert(test_shortest_path('^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$', 23))
     assert(test_shortest_path('^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$', 31))
