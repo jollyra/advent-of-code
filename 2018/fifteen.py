8d90642286885c56e009461d35c74b98935d9e3d
Nigel Rahkola <me@nigelrahkola.com>
2019-11-29 06:22:24 -0500

    get_path is working

--- 2018/fifteen.py
+++ 2018/fifteen.py
@@ -3,12 +3,23 @@ from collections import deque
 
 
 def new_grid(filename):
-    return inputs(filename)
+    grid = []
+    for line in inputs(filename):
+        row = []
+        for tile in line:
+            if tile in 'EG':
+                row.append((tile, 3, 200))
+            else:
+                row.append((tile))
+        grid.append(row)
+    return grid
 
 
 def render_grid(grid):
     for row in grid:
-        print(row)
+        for x in row:
+            print(x[0], end='')
+        print()
 
 
 def reading_order_neighbours4(point):
@@ -24,6 +35,16 @@ def get(grid, p):
     return grid[Y(p)][X(p)]
 
 
+def get_path(paths, p):
+    path = []
+    while paths[p]:
+        path.append(p)
+        p = paths[p]
+    path.append(p)
+    path.reverse()
+    return path
+
+
 def reading_order_bfs(grid, src, target):
     horizon = deque()
     horizon.append(src)
@@ -36,13 +57,12 @@ def reading_order_bfs(grid, src, target):
         for n in reading_order_neighbours4(cur):
             if in_bounds(grid, n):  # TODO move this logic out of here
                 tile = get(grid, n)
-                if tile == '.' and n not in path:
+                if tile[0] == '.' and n not in path:
                     horizon.append(n)
                     path[n] = cur
-                if tile == target:
+                if tile[0] == target:
                     path[n] = cur
-                    print('path', path)
-                    return path
+                    return get_path(path, n)
     return None
 
 
@@ -50,7 +70,7 @@ def main():
     grid = new_grid('15_test.in')
     render_grid(grid)
     path = reading_order_bfs(grid, (1, 1), 'G')
-    print(path)
+    print('path:', path)
 
 
 if __name__ == '__main__':
