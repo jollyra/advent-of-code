8d37db5795218f7480aa190fb022505f7e28e0b9
Nigel Rahkola <me@nigelrahkola.com>
2019-12-03 07:38:52 -0500

    part 1

---
+++ 2019/three.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+
+from util import *
+
+
+directions = {
+    'R': (1, 0),
+    'U': (0, 1),
+    'L': (-1, 0),
+    'D': (0, -1),
+}
+
+
+def add_points(p, q):
+    return p[0] + q[0], p[1] + q[1]
+
+
+def trace_path(ds):
+    cur = (0, 0)
+    path = [cur]
+    for d in ds:
+        direction = d[0]
+        vec = directions[direction]
+        steps = int(d[1:])
+        for _ in range(steps):
+            cur = add_points(cur, vec)
+            path.append(cur)
+
+    return path
+
+
+def find_intersects(w1, w2):
+    s1 = set(w1[1:])
+    s2 = set()
+    intersects = []
+    for p in w2[1:]:
+        if p in s1:
+            intersects.append(p)
+        s2.add(p)
+    return intersects
+
+
+def manhattan_distance(p, q):
+    return (abs(X(p) - X(q)) + abs(Y(p) - Y(q)))
+
+
+def main():
+    print('pass')
+    wires = input_seqs('3.in', ',')
+    print(wires)
+    ws = [trace_path(d) for d in wires]
+    intersects = find_intersects(*ws)
+    dists = [manhattan_distance(i, (0, 0)) for i in intersects]
+    print(min(dists))
+
+
+if __name__ == '__main__':
+    main()
